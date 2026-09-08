4931aa7c80b8dca57572f68cde7e0e0123979e8a
DeepKumar7106 <konata7106@gmail.com>
2026-09-08 21:54:55 +0530

    fixed state data

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -2,10 +2,13 @@ import { useState } from "react";
 import Scoreboard from "./Scoreboard";
 import HomeHero from "./HomeHero";
 import GameTweaker from "../GameTweaker";
+import { useAuth } from "../../context/AuthContext";
 
 export default function Home() { 
+    const { user, isAuthenticated } = useAuth()
+
     const [gameDetails, setGameDetails] = useState({
-        "playerName" : "Konata", // hard coded for the testing puprose
+        "playerName" :  isAuthenticated ? user.username : "Konata", // hard coded for the testing puprose
         "opponent" : "cpu", // default will be against the bot
         "gameMode": "singlePlayer", // default mode
         "ballCount": 15, 
