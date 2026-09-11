9c86ab8fa130d1c56ec2182a145bbddf92e809ee
DeepKumar7106 <konata7106@gmail.com>
2026-09-11 16:35:20 +0530

    game page restructured

--- frontend/src/pages/Game.jsx
+++ frontend/src/pages/Game.jsx
@@ -1,14 +1,25 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Player from "../components/Player"
 import { checkStrike, getOpponentInput, checkWinner } from "../scripts/game.utils";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { useAuth } from "../context/AuthContext"
+import { postScore } from "../scripts/game.api"
+
+// const handlePostScore = async (scoreDetails) {
+//     try {
+//         const result = await postScore(scoreDetails)
+//     } catch (error) {
+//         console.error("Error:", err.message)
+//     }
+// }
+
 export default function Game() {
     // destructuring the useAuth
     const { user, isAuthenticated } = useAuth()
-    
     // store player details recieved from /home
     const location = useLocation()
+    // redirecting hook
+    const navigate = useNavigate()
 
     // default values to prevent crash during loading via URL
     // no data from the home :(
@@ -16,25 +27,24 @@ export default function Game() {
         playerName: user?.username || "Konata",
         opponent: "cpu",
         ballCount: 15,
-        inningMode: "batting"
+        inningMode: "batting",
+        isOnline: false,
+        gameId: null
     };
 
     // fetches the game information from the home
     const gameDetails = location.state || defaultGameDetails
+    const maxBalls = gameDetails.ballCount
 
-    // redirecting hook
-    const navigate = useNavigate()
 
     // if invalid data throw error screen
     if (!gameDetails) return <p>No data found.</p>;
     
-    // basic game variables
+    // basic game states
     const [playerInput, setPlayerInput] = useState(0)
     const [oppnInput, setOppnInput] = useState(0)
     const [strikeCount, setStrikeCount] = useState(0)
-    const maxBalls = gameDetails.ballCount
     const [ballCount, setBallCount] = useState(maxBalls) 
-    
     // name is hardcoded for testing, future note to update them over params
     const [player, setPlayer] = useState({
         "name": gameDetails.playerName,
@@ -45,7 +55,6 @@ export default function Game() {
         "name":gameDetails.opponent,
         "score":0
     })
-    
     // gameplay variables
     const [gameplayMode, setGameplayMode] = useState(true) // defines whether the input is allowed 
     const [mode, setMode] = useState(gameDetails.inningMode) // batting or balling
@@ -54,106 +63,148 @@ export default function Game() {
     const [inningEndString, setInningEndString] = useState("") // defines the UI for each inning
     const [ctaButton, setCtaButton] = useState("Continue") // defines the button on the display
     
+    const processTurn = (playerInput, opponentInput) => {
+        // set both players input
+        setPlayerInput(playerInput)
+        setOppnInput(opponentInput)
+
+        // check for strike
+        const isStrike = checkStrike(playerInput, opponentInput)
+        
+        if (isStrike) {
+            setStrikeCount(count => count + 1)
+        } else {
+            setStrikeCount(0)
+
+            if (mode == "batting") {
+                setPlayer((prev) => {
+                    const newScore = prev.score + playerInput
+                    checkTargetReached(newScore, opponent.score)
+                    return {
+                        ...prev,
+                        score: newScore
+                    }
+                })
+            } else {
+                setOpponent((prev) => {
+                    const newScore = prev.score + opponentInput
+                    checkTargetReached(player.score, newScore)
+                    return {
+                        ...prev,
+                        score: newScore
+                    }
+                })   
+            }
+        }
+
+        setBallCount((count) => count - 1)
+    }
+
+    const checkTargetReached = (playerScore, opponentScore) => {
+        if (inningCount === 1) {
+            const currentBatterScore = mode === "batting" ? playerScore : opponentScore
+            console.log(currentBatterScore)
+            if (currentBatterScore >= target) {
+                const winner = mode === "batting" ? player.name : opponent.name
+                setInningEndString(`${winner} won the game`)
+                setGameplayMode(false)
+            }
+        }
+    }
     // the actaul gameplay
     const handlePlayerClick = (num) => {
         try {
+            if (!gameplayMode) return
+
             if (ballCount === 1 || strikeCount === 2) {
-                // one inning has came to an end, reset the variables
-                // setStrikeCount(0)
-                setGameplayMode(false)
-
-                // set the target
-                setTarget(mode === "batting" ? player.score : opponent.score)
-
-                // update the inningEndString 
-                const currentBatter = mode === "balling" ? opponent : player
-                const nextBatter = mode === "batting" ? opponent : player
-                let inningRemarks = `You have to ${nextBatter.name === player.name ? 'chase' : 'defend'} ${currentBatter.score} in ${maxBalls}`
-
-                if (inningCount && strikeCount === 2) {
-                    // for the singleplayer 
-                    // for online 1v1 there may have to be different way to evaluate the winner
-                    const winner = mode === "batting" ? opponent.name : player.name
-                    inningRemarks = `${winner} won the game` 
-                }
-                setInningEndString(inningRemarks)
+                handleInningEnd()
+                return
             }
-            // store player input
-            const playerInputValue = num
-            
-            // get opponent input
-            const oppnInputValue = getOpponentInput(opponent.name)          
-            
-            // future note: when implenting game over network, should add asynchronous opperations on the following
-            // update the display values
-            if (oppnInputValue) {
-                setPlayerInput(playerInputValue)
-                setOppnInput(oppnInputValue)
-            }
-            // update scores
-            // check whether a strike or not
-            const isStrike = checkStrike(playerInputValue, oppnInputValue)
-            if (isStrike) 
-                setStrikeCount(count => count + 1)
-            else {
-                setStrikeCount(0)
-                // update the scores
-                if (mode == "batting") {
-                    setPlayer({
-                        ...player,
-                        score: player.score + playerInputValue
-                    })
-                } else {
-                    setOpponent({
-                        ...opponent,
-                        score: opponent.score + oppnInputValue
-                    })
-                }
-                
-                // check for win
-                if (inningCount) {
-                    const currentScore = mode === "batting" ? player.score : opponent.score
-                    const currentInput = mode === "batting" ? playerInputValue : oppnInputValue
-                    if (currentScore + currentInput > target) {
-                        const winner = mode === "batting" ? player.name : opponent.name
-                        const remarks = `${winner} won the game`
-                        setInningEndString(remarks)
-                        setGameplayMode(false)
-                    }
-                }
 
+            if (!gameDetails.isOnline) {
+                const cpuMove = Math.floor(Math.random() * 6) + 1
+                processTurn(num, cpuMove)
+            } else {
+                // online mode
             }
-            setBallCount(count => count - 1)
         } catch (error) {
             console.log(error)
         }
     }
 
+    useEffect(() => {
+        if (!gameDetails.isOnline) return
+    }, [gameDetails.isOnline])
+
+    const handleInningEnd = () => {
+        if (inningCount === 1) {
+            const winner = (player.score > opponent.score) ? player : opponent
+            setInningEndString(`${winner.name} won the game`)
+            setGameplayMode(false)
+        } else {
+            setGameplayMode(false)
+            const currentPlayerScore = (mode === "batting" ? player.score : opponent.score)
+            const target = Number(currentPlayerScore) + 1
+            setTarget(target)
+            
+            const currentBatter = mode === "balling" ? opponent : player
+            const nextBatter = mode === "batting" ? opponent : player
+            let inningRemarks = `You have to ${nextBatter.name === player.name ? 'chase' : 'defend'} ${currentBatter.score} in ${maxBalls}`
+            
+            setInningEndString(inningRemarks)
+        }
+    }
+
     const handleContinue = () => {
         if (ctaButton === "home") {
+            // post to score
+            const curretBallCount = Number(maxBalls) - Number(ballCount)
+            const scoreRequirements = {
+                "playerID" : user.id,     
+                "gameID" : gameDetails.gameId,           
+                "inningNumber" : 2,
+                "ballsPlayed" : curretBallCount,
+                "totalScore" : player.score
+            }
+
+            handlePostScore(scoreRequirements)
+            
             // redirect to home page
             navigate('/')
+            return
         }
-        // reset the ball count 
+
+        // reset the innign variables and allow the player input
         setBallCount(maxBalls)
-        
-        // allow the player input
         setGameplayMode(true)
         setStrikeCount(0)
-
-        // update the ctaButton state
         setCtaButton("home")
-
-        // switch the inning mode 
         setMode(prevMode => prevMode === "batting" ? "balling" : "batting" ) // i am stoopid
+        if (!inningCount) setInningCount(1)
+    }
 
-        if (!inningCount) 
-            setInningCount(1)
+    const handlePostScore = async (scoreDetails) => {
+        try {
+            const result = await postScore(scoreDetails)
+
+            const data = await result.json()
+
+            if (!result.ok) {
+                throw new Error(data.error || "Score upload failed");
+            }
+        } catch (error) {
+            console.error("Error:", err.message)
+        }
+    }
+
+    if (!isAuthenticated) {
+        navigate('\login')
+        return
     }
 
     return (
         <>
-            {isAuthenticated && <main className="game-main">
+            <main className="game-main">
                 {/* current player stats */}
                 <Player player={player}></Player>
 
@@ -178,15 +229,9 @@ export default function Game() {
                                 <p>
                                     {gameplayMode ? `Balls Remaining: ${ballCount}` : inningEndString}
                                 </p>
-                                <p >
-                                    {!gameplayMode ? <span
-                                                        onClick={handleContinue}
-                                                    >
-                                                        {ctaButton}
-                                                    </span> 
-                                                    : ""
-                                    }
-                                </p>
+                                {!gameplayMode && <button onClick={handleContinue}>
+                                    {ctaButton}
+                                </button>}
                             </div>
                             <div className="game-section__input__details__display player">{oppnInput}</div>
                         </div>
@@ -207,15 +252,7 @@ export default function Game() {
 
                 {/* opponent player stats */}
                 <Player player={opponent}></Player>
-            </main>}
+            </main>
         </>
     )
 }
-
-// {!gameplayMode && <div className="game-section__input__interval">
-//     {/* depending on the mode the player name will change */}
-//     <p>{inningEndString}</p>
-//     {!inningCount && <button
-//         onClick={handleContinue}
-//     >Continue</button>}
-// </div>}
---
+++ frontend/src/scripts/game.api.js
@@ -0,0 +1,16 @@
+export const postScore = async ( {playerID, gameID, inningNumber, ballsPlayed, totalScore} ) => {
+    const res = await fetch('http://localhost:5000/api/game/score', {
+        method: "POST",
+        headers: {"Content-Type" : "application/json",},
+        body : JSON.stringify({ playerID, gameID, inningNumber, ballsPlayed, totalScore })
+    })
+
+    const data = await res.json()
+
+    if (!res.ok) {
+        throw new Error(data.error || "Failed to submit game result.");
+    }
+
+    // update in future
+    return data
+}
